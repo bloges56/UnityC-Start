2dd854c291b1c5c35918b6aa15d80aead60abfc4
bloges56 <brady.drew.logan@gmail.com>
2023-03-22 13:45:36 -0500

    finished

---
+++ Assets/Configs/AttractionConfig.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Attraction Config", menuName ="Configs/AttractionConfig", order = 0)]
+public class AttractionConfig : ScriptableObject
+{
+    public string id;
+    public string title;
+    public string author;
+    public string description;
+    public string location;
+
+    public Sprite image;
+    public Vector2 thumbnailSize;
+    public Vector3 thumbnailPostiiton;
+
+    public Vector2 headerImageSize;
+    public Vector3 headerImagePosition;
+}
--- Assets/Scripts/MuseumApp/AttractionScreen.cs
+++ Assets/Scripts/MuseumApp/AttractionScreen.cs
@@ -1,11 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace MuseumApp
 {
     public class AttractionScreen : MonoBehaviour
     {
-     
+        public Image cover;
+
+        public TMP_Text attractionTitle;
+        public TMP_Text attractionLocation;
+        public TMP_Text attractionAuthor;
+        public TMP_Text attractionDescription;
+
+        public List<Image> stars;
+        public Color activeStarColor = new Color(1f, 0.75f, 0f);
+        public Color inactiveStarColor = new Color(0.78f, 0.78f, 0.79f);
+
+        private AttractionScreenParameters attractionScreenParameters;
+
+        public void OnClickBack()
+        {
+            Destroy(attractionScreenParameters.gameObject);
+            SceneManager.LoadScene("HomeScreen", LoadSceneMode.Single);
+        }
+
+        public void OnClickStar(int index)
+        {
+            PlayerPrefs.SetInt(attractionScreenParameters.attractionConfig.id, index);
+            SetupStars(index);
+        }
+
+        private void Start()
+        {
+            attractionScreenParameters = FindObjectOfType<AttractionScreenParameters>();
+            var attractionConfig = attractionScreenParameters.attractionConfig;
+
+            attractionTitle.text = attractionConfig.title;
+            attractionLocation.text = attractionConfig.location;
+            attractionAuthor.text = attractionConfig.author;
+            attractionDescription.text = attractionConfig.description;
+
+            SetupCover(attractionConfig);
+            SetupStars(PlayerPrefs.GetInt(attractionConfig.id));
+        }
+
+        private void SetupCover(AttractionConfig attractionConfig)
+        {
+            cover.sprite = attractionConfig.image;
+
+            var rectTransform = cover.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition3D = attractionConfig.headerImagePosition;
+            rectTransform.sizeDelta = attractionConfig.headerImageSize;
+        }
+
+        private void SetupStars(int activeStarCount)
+        {
+            for(int i = 0; i < stars.Count; i++)
+            {
+                stars[i].color = i < activeStarCount ? activeStarColor : inactiveStarColor;
+            }
+        }
     }
 }
